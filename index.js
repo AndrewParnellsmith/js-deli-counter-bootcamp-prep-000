78e76608b5dfd587ec196e37c0003687d342c817
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-30 04:31:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
 }
 function nowServing(deliLine) {
   if (deliLine.length === 0) {return ("There is nobody waiting to be served!") 
-  } else { return (`Currently serving ${deliLine[0]}.`)
+  } else { return (`Currently serving ${deliLine.shift()}.`)
   deliLine.shift() 
   return deliLine
 }

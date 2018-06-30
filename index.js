d483aebf8db964a2ef9d8261d3e6a00f24c6bd6c
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-30 06:16:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(deliLine) {
 }
 }
 function currentLine(line) {
-  
+  for( let )
   
   if ( line.length === 0 ) { return ("The line is currently empty.")
   } else { return (`The line is currently: `) 
